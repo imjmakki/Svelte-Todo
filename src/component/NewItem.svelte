af36c9a5540f56b94a1a8e1c4677d39ecaaccd9d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-07 15:34:18 +0300

    Svelte Todo

--- src/component/NewItem.svelte
+++ src/component/NewItem.svelte
@@ -1,11 +1,13 @@
 <script>
-    function handeKeyUp() {
+    function handeKeyUp({key}) {
+        if () {
+        }
     }
 
     let value = "";
 </script>
 
-<input type="tel" on:keyup={handeKeyUp} placeholder="Add Item" />
+<input type="tel" on:keyup={handeKeyUp} bind:value placeholder="Add Item" />
 
 <style>
 
