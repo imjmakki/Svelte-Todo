89090812b39fc83289a08e94c27bbb4186357587
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-07 13:49:02 +0300

    Svelte Todo

--- src/ToDoAPI.js
+++ src/ToDoAPI.js
@@ -2,4 +2,8 @@ export default class ToDoAPI {
     static async getAll() {
         return JSON.parse(localStorage.getItem("todo-list") || "[]");
     }
+
+    static async save(items) {
+
+    }
 }
