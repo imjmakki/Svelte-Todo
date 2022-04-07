b996b7de733db99f0bf622a70bfb6adc892def5e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-07 13:50:40 +0300

    Svelte Todo

--- src/stores.js
+++ src/stores.js
@@ -1,15 +1,3 @@
 import { writable } from "svelte/store";
 
-export const items = writable([
-    {
-        id: 1,
-        text: "Take out the practice",
-        completed: false
-    },
-    {
-        id: 2,
-        text: "Finish up the project",
-        completed: true
-
-    }
-]);
+export const items = writable([]);
