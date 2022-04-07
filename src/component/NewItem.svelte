25105023ebb30baebeb75d84fb9394556acd3d76
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-07 15:37:34 +0300

    Svelte Todo

--- src/component/NewItem.svelte
+++ src/component/NewItem.svelte
@@ -1,6 +1,8 @@
 <script>
     import { createEventDispatcher } from "svelte";
 
+    const dispatch = createEventDispatcher();
+
     let value = "";
     function handeKeyUp({key}) {
         if (key !== "Enter" || value.length === 0) return;
